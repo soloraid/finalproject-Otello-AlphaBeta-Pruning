6b2887f0c36ffa9d75bef94ec3c9cbaa4f40a955
soloraid <hm.praw@yahoo.com>
2019-01-14 15:05:05 +0330

    alphaBetaPruning function 1

--- final.c
+++ final.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-void detect( char boardGame[][8] , int argc , int selection[][2] );
+void detect( char boardGame[][8] , int argc , int selection[][2] , char player );
 void alphaBetaPruning( char boardGame[][8] , int selection[][2] , int depth , int nextMove[][2] ,
- int score[][8] , char player , int max );
-void newBoardGame( char boardGame[][8] );
+ int score[][8] , char player , int *maxPtr , int *minPtr );
+void newBoardGame( char boardGame[][8] , char player);
 
 int main(int argc, char const *argv[])
 {   
     char boardGame[argc][argc];
     int selection[30][2];
     int nextMove[1][2];
+    int *maxPtr , *minPtr ;
     int depth = 5;
+    char player = '1' ;
     int score[8][8]={
         {9,0,8,5,5,8,0,9},
         {0,1,5,6,6,5,1,0},
@@ -20,6 +22,8 @@ int main(int argc, char const *argv[])
         {0,1,5,6,6,5,1,0},
         {9,0,8,5,5,8,0,9},
     };
+    *maxPtr = 0;
+    *minPtr = 576;
 
     for(int i = 0 ; i<argc-2 ; i++)
     {
@@ -28,7 +32,7 @@ int main(int argc, char const *argv[])
             boardGame[i][j]=argv[i+1][j];
         }
     }
-    detect( boardGame , argc-2 , selection );
+    detect( boardGame , argc-2 , selection , player );
     return 0;
 }
 
@@ -40,21 +44,14 @@ int main(int argc, char const *argv[])
 
 }*/
 
-void alphaBetaPruning( char boardGame[][8] , int selection[][2] , int depth , int nextMove[][2] , int score[][8] , char player , int max );
+void alphaBetaPruning( char boardGame[][8] , int selection[][2] , int depth , int nextMove[][2] , int score[][8] , char player , int *maxPtr , int *minPtr )
 {   
     int counterA=0;
     int counterB=0;
-    
+
     if ( depth!=0 )
     {
-        for(int  i = 0 ; i<30 ; i++ )
-        {
-            if( selection[i][0]==-1 )
-            break;
-            boardGame[selection[i][0]][selection[i][1]] = player ;
-            newBoardGame(boardGame);
 
-        }
     }
 }
 
